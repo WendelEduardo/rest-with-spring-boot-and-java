91fcf611b0ceea2dca62415ac3ed9ac603eeac00
WendelEduardo <wendeleduardo2002@gmail.com>
2023-04-07 12:20:44 -0300

    First Steps in Java with Spring Boot 3

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/udemy/restwithspringbootandjava/GreetingController.java
@@ -0,0 +1,18 @@
+package br.com.udemy.restwithspringbootandjava;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello %s!";
+    private static final AtomicLong count = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(count.incrementAndGet(), String.format(template, name));
+    }
+}
